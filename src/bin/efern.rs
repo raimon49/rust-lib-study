11303f3498817fe82fd6448e234789c6418290e3
raimon <raimon49@hotmail.com>
2020-09-15 20:06:48 +0900

    Add function and test code

--- src/bin/efern.rs
+++ src/bin/efern.rs
@@ -9,3 +9,13 @@ fn main() {
     run_simuration(&mut fern, 1000);
     println!("final fern size: {}", fern.size);
 }
+
+fn rouchly_equal(a: f64, b: f64) -> bool {
+    (a - b).abs() < 1e-6
+}
+
+#[test]
+fn trig_works() {
+    use std::f64::consts::PI;
+    assert!(rouchly_equal(PI.sin(), 0.0));
+}
