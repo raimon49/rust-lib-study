a4878955b8d68a197dc0cde2ad46cdca4b48a10b
raimon <raimon49@hotmail.com>
2020-09-15 20:09:41 +0900

    Define module for only run tests

--- src/bin/efern.rs
+++ src/bin/efern.rs
@@ -10,12 +10,18 @@ fn main() {
     println!("final fern size: {}", fern.size);
 }
 
-fn rouchly_equal(a: f64, b: f64) -> bool {
-    (a - b).abs() < 1e-6
-}
+// #[cfg]属性による条件付きコンパイルにより、cargo testの時しかコンパイルされない
+// testsモジュールとして囲っておくことでcargo runした際に
+// 関数rouchly_equalが呼ばれないエラーの発生を避けることができる
+#[cfg(test)]
+mod tests {
+    fn rouchly_equal(a: f64, b: f64) -> bool {
+        (a - b).abs() < 1e-6
+    }
 
-#[test]
-fn trig_works() {
-    use std::f64::consts::PI;
-    assert!(rouchly_equal(PI.sin(), 0.0));
+    #[test]
+    fn trig_works() {
+        use std::f64::consts::PI;
+        assert!(rouchly_equal(PI.sin(), 0.0));
+    }
 }
