558288b4b15a77e6e454f4c9f93574694efe3e28
raimon <raimon49@hotmail.com>
2020-09-17 20:40:40 +0900

    Add doctest for 'overlap'

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,11 @@ pub fn run_simuration(fern: &mut Fern, days: usize) {
     }
 }
 
+///
+/// Return true if two ranges overlap.
+///
+///     assert_eq!(fern_sim::overlap(0..7, 3..10), true);
+///     assert_eq!(fern_sim::overlap(1..5, 101..105), false);
 pub fn overlap(r1: Range<usize>, r2: Range<usize>) -> bool {
     r1.start < r1.end && r2.start < r1.end &&
         r1.start < r2.end && r2.start < r1.end
